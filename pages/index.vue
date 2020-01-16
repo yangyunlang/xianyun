0889875fd8d804fb84b5837981d45596210df485
梦幻 <web2018_lang@163.com>
2020-01-16 20:30:37 +0800

    轮播图存在bug

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,51 @@
 <template>
   <div class="container">
-    首页
+    <!-- 幻灯片 -->
+    <el-carousel :interval="5000" arrow="always">
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <div
+          class="banner-image"
+          :style="`
+                 background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat;
+             background-size:contain contain;
+                `"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      banners: [], // 轮播图数据
+    }
+  },
+  mounted () {
+    this.$axios({
+      url: "/scenics/banners"
+    }).then(res => {
+      const { data } = res.data;
+      this.banners = data;
+    })
+  }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
 
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
